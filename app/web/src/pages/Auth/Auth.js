c09b95551295657867b20b99dbc628942cde2937
Zoirjon <45711236+zoirj0n@users.noreply.github.com>
2023-02-24 14:59:23 -0500

    Update Auth.js

--- app/web/src/pages/Auth/Auth.js
+++ app/web/src/pages/Auth/Auth.js
@@ -14,7 +14,8 @@ export default function Auth() {
           height="60"
         />
         <h2 className="login-title">Welcome to, new intra</h2>
-        <a className="login-btn" href="/">
+        <a className="login-btn" href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-cb4d4617c93d88fb16730d34efc24e094657b0f972550ce8d7823f0d779ef738&redirect_uri=http%3A%2F%2F127.0.0.1&response_type=code
+">
           log in
         </a>
       </div>
